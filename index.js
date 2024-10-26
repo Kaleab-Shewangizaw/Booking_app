c1f60317bbce1bdbaf083edb1a74e9632be8c85f
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-10-26 19:41:51 +0300

    mongoDB, mongoose, dotenv, nodemon

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connceted!");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello first request");
+});
+
+app.listen(3000, () => {
+  connect();
+  console.log("connected to 3000!");
+});
