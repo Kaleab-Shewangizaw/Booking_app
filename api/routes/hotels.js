4e2653aea5f56938dd956e6e69818548eb3fdcf6
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-10-27 21:43:16 +0300

    CRUD operation

---
+++ api/routes/hotels.js
@@ -0,0 +1,63 @@
+import express from "express";
+import Hotel from "../models/hotel.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  //here what we are doing is we are creating a new instance of the Hotel model and passing the request body to it
+
+  try {
+    const savedHotel = await newHotel.save();
+    //the use of .save() fucntion is to save the data to the database and it returns a promise, a promise is a way to handle asynchronous code in javascript
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//DELATE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL
+
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
