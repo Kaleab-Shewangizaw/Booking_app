153a3aa0048d25ff346ce25b2436e534bbfd398a
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-11-02 10:23:30 +0300

    added rooms api

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,22 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  updateRoom,
+  getAllRooms,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+router.get("/:id", getRoom);
+
+router.get("/", getAllRooms);
 export default router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,33 +5,39 @@ import {
   getUser,
   updateUser,
 } from "../controllers/user.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // GET
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.send("Hello user, You are logged in!");
-});
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, You are logged in!");
+// });
 
-router.get("/checkuser", verifyUser, (req, res, next) => {
-  res.send(
-    "Hello user, You are logged in and you can delete or update your profile!"
-  );
-});
+// router.get("/checkuser", verifyUser, (req, res, next) => {
+//   res.send(
+//     "Hello user, You are logged in and you can delete or update your profile!"
+//   );
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send(
+//     "Hello admin, you are logged in and you can delete or update any user profile!"
+//   );
+// });
 
 //CREATE
 
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 //DELATE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //GET ALL
 
-router.get("/", getAllUsers);
+router.get("/", verifyUser, getAllUsers);
 
 export default router;
